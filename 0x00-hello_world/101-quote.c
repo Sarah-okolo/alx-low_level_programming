22590b841a26aed1248b3271899488a14e1bd293
Sarah-okolo <okolosarah402@gmail.com>
2022-09-11 12:48:50 -0700

    modified 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,13 @@
 /**
  *main - Entry point
  *
- *Return: Always 0 (Success)
+ *Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	char *mgs = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+
+	write(STDOUT_FILENO, mgs, 59);
+
 	return (1);
 }
