2c1efe31658cf9bb9fe307d90945165f2728bdde
Sarah-okolo <okolosarah402@gmail.com>
2022-09-14 14:19:44 -0700

    created the file 0-main.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,7 +4,6 @@
  *main - tests function that prints if integer is positive or negative
  *Return: 0
  */
-
 int main(void)
 {
 	int i;
@@ -12,6 +11,8 @@ int main(void)
 	i = 98 % 2;
 
 		positive_or_negative(i);
+		positive_or_negative('\n')
+
 
 		return (0);
 }
