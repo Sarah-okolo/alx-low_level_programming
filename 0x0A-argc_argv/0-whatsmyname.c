91f522b9ebc05c63cfd6531a2df9816043db1aee
Sarah-okolo <okolosarah402@gmail.com>
2022-09-30 16:24:25 +0100

    modified the file 0-whatsmyname.c to fit Betty's standard

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 /**
- * main-prints this files' name
+ * main - prints this files' name 
+ * @argc: argument count
+ * @argv: argument vector
  *
- * return-Always return 0(Success)
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char* argv[])
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 
-	return 0;
+	return (0);
 }
