86f1d7137949a844be067832d136404bbe4fd830
sahra <okolosarah402@gmail.com>
2022-09-30 16:40:26 +0100

    Create 0x1-args.c

---
+++ 0x0A-argc_argv/0x1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed
+ * @argc: command line argument count
+ * @argv: command line argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)*argc[];
+
+	printf("%d\n", argc);
+
+	return (0);
+}
